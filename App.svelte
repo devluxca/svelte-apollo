6074a11ab796354293c42913fbe958e3e4ee3e8f
Lucas <luscasimoni@gmail.com>
2022-04-15 21:27:34 -0300

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,21 @@
+<script>
+ import Todos from "./Todos.svelte";
+ import ApolloClient from "apollo-boost";
+ import { setClient } from "svelte-apollo";
+ const client = new ApolloClient({
+   uri: "https://todos-graphql.herokuapp.com/graphql"
+ });
+ setClient(client);
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+
+<main>
+	<h1>Svelte Todos</h1>
+  <Todos />
+</main>
